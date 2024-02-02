8e6344496cb8e02ec09927c76001eb4761c14b19
Aditya Shankar Khorne <153326893+adityaSkhorne2401@users.noreply.github.com>
2024-02-02 16:46:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "AdityaSkhorne2401",
+  title: "Web Designer",
+  email: "adityashankarkhorne@gmail.com",
+  gitHub: "adityaSkhorne2401",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
